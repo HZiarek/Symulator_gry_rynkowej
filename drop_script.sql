c3059b8a522666a577f0c188885df92107665e69
Huzar <huzar@github.com>
2019-06-04 11:58:20 +0200

    Main ddl script

---
+++ drop_script.sql
@@ -0,0 +1,8 @@
+--usuwanie wszystkiego
+drop table badanie_rynku;
+drop table konsument;
+drop table koszt_magazynowania;
+drop table licznik_rund;
+drop table utowrzona_marka;
+drop table producent;
+drop table ustawienia_poczatkowe;
